a2027c87f5c1e6b9929fe9d8c4d6561da9c90a4d
Eric Gerlach <eric@gerlach.ca>
2017-01-07 16:39:02 -0500

    Trying to get startup with injected conf file happening

--- deluged/start.sh
+++ deluged/start.sh
@@ -1,8 +1,16 @@
-SALT=`cat Saltfile`
+#!/bin/bash
+
+CONFIG=/var/lib/deluged/config/
+SALT=`cat $CONFIG/Saltfile`
 if [ -z $SALT ]; then
-	SALT=`< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32} > Saltfile`
-	SALT=`cat Saltfile`
+  SALT=`< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32} > $CONFIG/Saltfile`
+  SALT=`cat $CONFIG/Saltfile`
 fi
+
 HASHED_PASSWD=`echo -n "$SALT$PASSWD" | sha1sum | awk '{ print $1}'`
 
-cat core.conf | jq "if has(\"pwd_sha1\") then .pwd_sha1=\"$HASHED_PASSWD\" else . end"
+$(cat $CONFIG/core.conf | \
+  jq "if has(\"pwd_sha1\") then .pwd_sha1=\"$HASHED_PASSWD\" else . end" | \
+  jq "if has(\"pwd_sha1\") then .pwd_sha1=\"$HASHED_PASSWD\" else . end") > $CONFIG/core.conf
+
+/usr/bin/deluged -d -Ldebug -c$CONFIG
