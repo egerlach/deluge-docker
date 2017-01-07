1d463c2eebfc5eb4a890c477ba5e13dd427dd534
Eric Gerlach <eric@gerlach.ca>
2017-01-07 18:32:20 -0500

    Working core.conf injection

--- deluged/start.sh
+++ deluged/start.sh
@@ -7,10 +7,14 @@ if [ -z $SALT ]; then
   SALT=`cat $CONFIG/Saltfile`
 fi
 
+if [ ! -f $CONFIG/core.conf ]; then
+  cp /core.conf $CONFIG
+fi
+
 HASHED_PASSWD=`echo -n "$SALT$PASSWD" | sha1sum | awk '{ print $1}'`
 
-$(cat $CONFIG/core.conf | \
+cat $CONFIG/core.conf | \
   jq "if has(\"pwd_sha1\") then .pwd_sha1=\"$HASHED_PASSWD\" else . end" | \
-  jq "if has(\"pwd_sha1\") then .pwd_sha1=\"$HASHED_PASSWD\" else . end") > $CONFIG/core.conf
+  sponge $CONFIG/core.conf
 
 /usr/bin/deluged -d -Ldebug -c$CONFIG
