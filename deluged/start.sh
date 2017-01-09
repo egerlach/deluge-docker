ec0f0472eff464db0947e8a1225444e320f541bf
Eric Gerlach <eric@gerlach.ca>
2017-01-08 20:32:59 -0500

    Get deluge daemon running and responding to port

--- deluged/start.sh
+++ deluged/start.sh
@@ -11,10 +11,17 @@ if [ ! -f $CONFIG/core.conf ]; then
   cp /core.conf $CONFIG
 fi
 
-HASHED_PASSWD=`echo -n "$SALT$PASSWD" | sha1sum | awk '{ print $1}'`
+if [ ! -z "$TORRENT_PORT" ]; then
+  cat $CONFIG/core.conf | \
+    jq "if has(\"random_port\") then .random_port=false else . end" | \
+    jq "if has(\"listen_ports\") then .listen_ports=[$TORRENT_PORT,$TORRENT_PORT] else . end" | \
+    sponge $CONFIG/core.conf
+fi
+
+HASHED_PASSWD=`echo -n "$SALT$PASSWD" | sha1sum | awk '{ print $1 }'`
 
 cat $CONFIG/core.conf | \
   jq "if has(\"pwd_sha1\") then .pwd_sha1=\"$HASHED_PASSWD\" else . end" | \
   sponge $CONFIG/core.conf
 
-/usr/bin/deluged -d -Ldebug -c$CONFIG
+/usr/bin/deluged -d -c$CONFIG "$@"
