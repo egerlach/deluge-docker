32a7808b7bfeef505aea114b14adee47799d719b
Eric Gerlach <eric@gerlach.ca>
2017-01-28 21:11:05 -0500

    Use gosu to set user and group

--- deluged/start.sh
+++ deluged/start.sh
@@ -2,6 +2,12 @@
 
 CONFIG=/var/lib/deluged/config
 
+for d in "$DIRS"; do
+  mkdir -p /srv/$d
+done
+
+GOSU="/usr/local/bin/gosu $DELUGE_USER_ID:$DELUGE_GROUP_ID"
+
 if [ ! -f $CONFIG/core.conf ]; then
   cp /core.conf $CONFIG
 fi
@@ -10,11 +16,11 @@ if [ ! -z "$TORRENT_PORT" ]; then
   cat $CONFIG/core.conf | \
     jq "if has(\"random_port\") then .random_port=false else . end" | \
     jq "if has(\"listen_ports\") then .listen_ports=[$TORRENT_PORT,$TORRENT_PORT] else . end" | \
-    sponge $CONFIG/core.conf
+    $GOSU sponge $CONFIG/core.conf
 fi
 
 if ! grep -q "docker:$SHARED_SECRET" $CONFIG/auth; then
-  echo "docker:$SHARED_SECRET:10" >> $CONFIG/auth
+  $GOSU /bin/bash -c "echo \"docker:$SHARED_SECRET:10\" >> $CONFIG/auth"
 fi
 
-/usr/bin/deluged -d -c$CONFIG "$@"
+$GOSU /usr/bin/deluged -d -c$CONFIG "$@"
