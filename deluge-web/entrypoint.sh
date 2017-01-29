f58af7016b013773bf69409c80e267077917ac10
Eric Gerlach <eric@gerlach.ca>
2017-01-28 22:08:29 -0500

    Add gosu to deluge-web container

--- deluge-web/entrypoint.sh
+++ deluge-web/entrypoint.sh
@@ -2,21 +2,25 @@
 
 CONFIG=/var/lib/deluged/web-config
 
+chown $DELUGE_USER_ID:$DELUGE_GROUP_ID /var/lib/deluged /var/lib/deluged/config
+
+GOSU="/usr/local/bin/gosu $DELUGE_USER_ID:$DELUGE_GROUP_ID"
+
 if [ ! -f $CONFIG/core.conf ]; then
-  cp /core.conf $CONFIG
+  $GOSU cp /core.conf $CONFIG
 fi
 
 if [ ! -f $CONFIG/web.conf ]; then
-  cp /web.conf $CONFIG
+  $GOSU cp /web.conf $CONFIG
 fi
 
 if [ ! -f $CONFIG/hostlist.conf.1.2 ]; then
-  cp /hostlist.conf.1.2 $CONFIG
+  $GOSU cp /hostlist.conf.1.2 $CONFIG
 fi
 
 SALT=`cat $CONFIG/Saltfile`
 if [ -z $SALT ]; then
-  SALT=`< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32 > $CONFIG/Saltfile`
+  SALT=`< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32 | $GOSU sponge $CONFIG/Saltfile`
   SALT=`cat $CONFIG/Saltfile`
 fi
 
@@ -26,10 +30,10 @@ cat $CONFIG/web.conf | \
   jq "if has(\"pwd_sha1\") then .pwd_sha1=\"$HASHED_PASSWD\" else . end" | \
   jq "if has(\"pwd_salt\") then .pwd_salt=\"$SALT\" else . end" | \
   jq "if has(\"first_login\") then .first_login=\"false\" else . end" | \
-  sponge $CONFIG/web.conf
+  $GOSU sponge $CONFIG/web.conf
 
 cat $CONFIG/hostlist.conf.1.2 | \
   jq "if has(\"hosts\") then .hosts=[.hosts[] | if .[0]==\"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\" then [.[0,1,2,3], \"$SHARED_SECRET\"] else . end] else . end" | \
-  sponge $CONFIG/hostlist.conf.1.2
+  $GOSU sponge $CONFIG/hostlist.conf.1.2
 
-/usr/bin/deluge-web -c$CONFIG "$@"
+$GOSU /usr/bin/deluge-web -c$CONFIG "$@"
